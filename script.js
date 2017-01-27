4da137c79c3e8779c41b59b7154165a218234116
Anupam Majhi <majhi.anupam@gmail.com>
2017-01-28 03:08:57 +0530

    Add JS Functions Scaffolding

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ var isScrolledToBottom = summary.scrollHeight - summary.clientHeight <= summary.
 function scrollToBottom(){
   summary.scrollTop = summary.scrollHeight - summary.clientHeight;
 }
+
+var calcArr = [];
+
+// Scroll to bottom of display
+$(".btn").click(scrollToBottom);
+
+
+$(".num-btn").click(function(){
+
+});
+
+$(".op-btn").click(function(){
+
+});
+
+$(".btn-eql").click(function(){
+
+});
+
+$(".btn-misc").click(function(){
+  
+});
