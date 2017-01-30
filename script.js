dc49735bb53e820d709668b8bb58bce0a0ecfa78
Anupam Majhi <majhi.anupam@gmail.com>
2017-01-30 22:34:13 +0530

    Add basic functions

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-
 summary = $(".summaryOuter")[0]
 var isScrolledToBottom = summary.scrollHeight - summary.clientHeight <= summary.scrollTop + 1;
-function scrollToBottom(){
-  summary.scrollTop = summary.scrollHeight - summary.clientHeight;
+
+function scrollToBottom() {
+    summary.scrollTop = summary.scrollHeight - summary.clientHeight;
+}
+
+//Display Value in div opout or numout
+function display(val, outLoc) {
+    outClass = "." + outLoc
+    $(outClass).html(val)
 }
 
 var calcArr = [];
+var lastEntry = "";
+
+//Function Calc to Calculate
+function calc(leftOp,operator,rightOp){
+  switch(operator){
+    case '+': return leftOp + rightOp;
+    case '—': return leftOp - rightOp;
+    case 'x': return leftOp * rightOp;
+    case '÷': if(rightOp === 0){
+        return leftOp / rightOp;
+    }
+    else{
+      return "error"
+    }
+
+    default: console.log("Invalid Operation");
+  }
+}
 
 // Scroll to bottom of display
-$(".btn").click(scrollToBottom);
+$(".btn").click(function(){
+  scrollToBottom
+});
 
 
-$(".num-btn").click(function(){
+$(".num-btn").click(function() {
 
 });
 
-$(".op-btn").click(function(){
+$(".op-btn").click(function() {
 
 });
 
-$(".btn-eql").click(function(){
+$(".btn-eql").click(function() {
 
 });
 
-$(".btn-misc").click(function(){
-  
+$(".btn-misc").click(function() {
+
 });
